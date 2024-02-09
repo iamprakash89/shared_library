efe52bf6f349cb18a35959e58732849bf2fe0a5d
Prakash Ashokan <prakash.ashokan@rackspace.com>
2024-02-09 15:08:00 +0530

    docker build

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,7 +1,9 @@
 def call(String projectname, String Imgname, String DuserUser){
     sh """
      docker image build -t ${DuserUser}:${projectname} .
+     echo "img tag ${DuserUser}:${projectname} ${DuserUser}:${projectname}:${Imgname}"
      docker image tag ${DuserUser}:${projectname} ${DuserUser}:${projectname}:${Imgname}
+     echo "tagging ${DuserUser}:${projectname} ${DuserUser}:${projectname}:latest"
      docker image tag ${DuserUser}:${projectname} ${DuserUser}:${projectname}:latest 
     """
 }
