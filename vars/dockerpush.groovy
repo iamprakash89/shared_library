2921fc041d95424f2cc297ed10222af8ddc0ea40
Prakash Ashokan <prakash.ashokan@rackspace.com>
2024-02-09 22:06:30 +0530

    docker push

---
+++ vars/dockerpush.groovy
@@ -0,0 +1,11 @@
+ def call(String project, String imgtag, String user){
+        withCredentials([usernamePassword(
+            credentialsId: "docker",
+            usernameVariable: "USER",
+            passwordVariable: "PASS"
+    )]) {
+        sh "docker login -u '$USER' -p '$PASS'"
+    }
+    sh "docker image push ${hubUser}/${project}:${ImageTag}"
+    sh "docker image push ${hubUser}/${project}:latest"   
+}
