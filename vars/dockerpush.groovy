0539fa10ec3482c6bfd55d622770119b5fdfa154
Prakash Ashokan <prakash.ashokan@rackspace.com>
2024-02-10 00:01:33 +0530

    docker push

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -1,11 +1,18 @@
- def call(String project, String imgtag, String user){
-        withCredentials([usernamePassword(
-            credentialsId: "docker",
-            usernameVariable: "USER",
-            passwordVariable: "PASS"
-    )]) {
-        sh "docker login -u '$USER' -p '$PASS'"
-    }
-    sh "docker image push ${user}/${project}:${imgtag}"
-    sh "docker image push ${user}/${project}:latest"   
+//  def call(String project, String imgtag, String user){
+//         withCredentials([usernamePassword(
+//             credentialsId: "docker",
+//             usernameVariable: "USER",
+//             passwordVariable: "PASS"
+//     )]) {
+//         sh "docker login -u '$USER' -p '$PASS'"
+//     }
+//     sh "docker image push ${user}/${project}:${imgtag}"
+//     sh "docker image push ${user}/${project}:latest"   
+// }
+
+def call(String project, String accountid, String region){
+    sh """
+       aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${accountid}.dkr.ecr.${region}.amazonaws.com
+       docker push ${accountid}.dkr.ecr.${region}.amazonaws.com/${project}:latest
+    """
 }
