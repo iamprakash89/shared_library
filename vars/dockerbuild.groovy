0794d084a9eadd2c310ba72245e354e58772273b
Prakash Ashokan <prakash.ashokan@rackspace.com>
2024-02-09 15:28:04 +0530

    quality gate

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,17 +1,19 @@
-// def call(String project, String Imgname, String DuserUser){
-//     sh """
+ def call(String project, String imgtag, String user){
+     sh """
 
-//      docker image build -t ${DuserUser}:${project} .
-//     """
-// }
+      docker image build -t ${user}:${project} .
+      docker iamge tag  ${user}:${project} ${user}:${project}:${imgtag}
+      docker iamge tag  ${user}:${project} ${user}:${project}:latest
+     """
+}
 //  // docker image build -t ${DuserUser}:${projectname} .
 //  //docker image tag ${DuserUser}:${projectname} ${DuserUser}:${projectname}:latest 
 
-def call(String project, String ImageTag, String hubUser){
+// def call(String project, String ImageTag, String hubUser){
     
-     sh """
-      docker image build -t ${hubUser}/${project} . 
-      docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
-      docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
-     """
- }
+//      sh """
+//       docker image build -t ${hubUser}/${project} . 
+//       docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+//       docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+//      """
+//  }
