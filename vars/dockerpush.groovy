295ef951f448a92beb1f3984c1520b31baec5b6c
Prakash Ashokan <prakash.ashokan@rackspace.com>
2024-02-10 00:29:12 +0530

    docker push

--- vars/dockerpush.groovy
+++ vars/dockerpush.groovy
@@ -11,10 +11,11 @@
 // }
 
 def call(String project, String accountid, String region){
+
     sh """
       
-       docker login -u AWS -p $(aws ecr get-login-password --region ap-south-1) ${accountid}.dkr.ecr.${region}.amazonaws.com
+       docker login -u AWS -p \$(aws ecr get-login-password --region ${region}) ${accountid}.dkr.ecr.${region}.amazonaws.com
        docker push ${accountid}.dkr.ecr.${region}.amazonaws.com/${project}:latest
     """
 } 
-//ws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${accountid}.dkr.ecr.${region}.amazonaws.com
+//aws ecr get-login-password --region ${region} | docker login --username AWS --password-stdin ${accountid}.dkr.ecr.${region}.amazonaws.com
