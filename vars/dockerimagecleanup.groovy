e2bf604ead7ea1e197e2c113d8074d419d66000a
Prakash Ashokan <prakash.ashokan@rackspace.com>
2024-02-10 00:07:40 +0530

    docker push

--- vars/dockerimagecleanup.groovy
+++ vars/dockerimagecleanup.groovy
@@ -1,8 +1,14 @@
- def call(String project, String imgtag, String user){
-     sh """
+//  def call(String project, String imgtag, String user){
+//      sh """
 
      
-      docker rmi   ${user}/${project}:${imgtag}
-      docker rmi   ${user}/${project}:latest
-     """
+//       docker rmi   ${user}/${project}:${imgtag}
+//       docker rmi   ${user}/${project}:latest
+//      """
+// }
+
+def call(String project, String accountid, String region){
+    sh """
+    docker rmi ${project}:latest ${accountid}.dkr.ecr.${region}.amazonaws.com/${project}:latest
+    """
 }
