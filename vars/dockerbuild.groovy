cd84adc38dc780bc10b445fca65fb3a0f8b052fc
Prakash Ashokan <prakash.ashokan@rackspace.com>
2024-02-09 23:25:36 +0530

    ecr build

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,8 +1,20 @@
- def call(String project, String imgtag, String user){
-     sh """
+//  def call(String project, String imgtag, String user){
+//     // For Docker Hub push
+//     //  sh """
 
-      docker image build -t ${user}/${project} .
-      docker image tag  ${user}/${project} ${user}/${project}:${imgtag}
-      docker image tag  ${user}/${project} ${user}/${project}:latest
-     """
+//     //   docker image build -t ${user}/${project} .
+//     //   docker image tag  ${user}/${project} ${user}/${project}:${imgtag}
+//     //   docker image tag  ${user}/${project} ${user}/${project}:latest
+//     //  """
+
+
+// }
+
+// ECR BUILD
+def call(String project, String accountid, String region){
+    sh """
+    docker build -t ${project} .
+    docker tag ${project}:latest ${accountid}.dkr.ecr.${region}.amazonaws.com/${project}:latest
+    """
+
 }
