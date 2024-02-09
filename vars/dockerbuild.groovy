d52d9a044a44536e72b2bd487c6300a0593f7d30
Prakash Ashokan <prakash.ashokan@rackspace.com>
2024-02-09 15:25:19 +0530

    quality gate

--- vars/dockerbuild.groovy
+++ vars/dockerbuild.groovy
@@ -1,8 +1,17 @@
-def call(String project, String Imgname, String DuserUser){
-    sh """
+// def call(String project, String Imgname, String DuserUser){
+//     sh """
 
-     docker image build -t ${DuserUser}:${project} -t ${DuserUser}:${project}:latest .
-    """
-}
- // docker image build -t ${DuserUser}:${projectname} .
- //docker image tag ${DuserUser}:${projectname} ${DuserUser}:${projectname}:latest 
+//      docker image build -t ${DuserUser}:${project} .
+//     """
+// }
+//  // docker image build -t ${DuserUser}:${projectname} .
+//  //docker image tag ${DuserUser}:${projectname} ${DuserUser}:${projectname}:latest 
+
+def call(String project, String ImageTag, String hubUser){
+    
+     sh """
+      docker image build -t ${hubUser}/${project} . 
+      docker image tag ${hubUser}/${project} ${hubUser}/${project}:${ImageTag}
+      docker image tag ${hubUser}/${project} ${hubUser}/${project}:latest
+     """
+ }
