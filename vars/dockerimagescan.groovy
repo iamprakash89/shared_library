d04d6ff52f937b9352523c2f88030338fe9d259b
Prakash Ashokan <prakash.ashokan@rackspace.com>
2024-02-09 23:49:41 +0530

    docker cleanup

--- vars/dockerimagescan.groovy
+++ vars/dockerimagescan.groovy
@@ -1,8 +1,8 @@
  def call(String project, String accountid, String region){
      sh """
       echo "${project}:latest ${accountid}.dkr.ecr.${region}.amazonaws.com/${project}:latest"
-    
-      
+      trivy image ${project}:latest ${accountid}.dkr.ecr.${region}.amazonaws.com/${project}:latest >scan.txt
+      cat scan.txt
      """
 }
 
